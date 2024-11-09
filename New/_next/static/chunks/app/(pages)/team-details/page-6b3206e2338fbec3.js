ee8964526964738971e995cf1902c5bb7511b20c
devlabscorporation <121150895+uaposalinas@users.noreply.github.com>
2024-11-09 06:02:16 -0600

    none

---
+++ New/_next/static/chunks/app/(pages)/team-details/page-6b3206e2338fbec3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[773,724,254,516,520,592,327],{3607:function(e,n,s){Promise.resolve().then(s.bind(s,8517)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[987,517,971,117,744],function(){return e(e.s=3607)}),_N_E=e.O()}]);
---
+++ New/_next/static/chunks/app/(pages)/testimonials/page-4fdf6547f3778c8f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[654],{8050:function(n,e,u){Promise.resolve().then(u.bind(u,8517))}},function(n){n.O(0,[987,517,971,117,744],function(){return n(n.s=8050)}),_N_E=n.O()}]);
